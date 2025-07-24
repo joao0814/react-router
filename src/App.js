b718562a2afc31ffc6ed01baec3b6ed358fccd98
joao0814 <joaopmfranca@gmail.com>
2025-07-24 20:19:23 -0300

    hello world

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Salve</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Opa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
