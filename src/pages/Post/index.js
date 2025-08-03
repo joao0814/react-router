29f448d20eb3e1fa9548338f6683a1d85ed2c1e4
joao0814 <joaopmfranca@gmail.com>
2025-08-03 11:23:33 -0300

    style: change markdown style

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,20 +1,38 @@
 import { useParams } from "react-router-dom";
 import posts from "../../json/posts.json";
 import PostModel from "components/PostModel";
+import ReactMarkdown from "react-markdown";
+import "./Post.css";
 
-export default function Post({}) {
+export default function Post() {
   const parametros = useParams();
 
-  const post = posts.find((post) => {
-    return post.id === Number(parametros.id);
-  });
+  const post = posts.find((post) => post.id === Number(parametros.id));
+
+  const componentesMarkdown = {
+    img: ({ src = "", alt = "" }) => {
+      try {
+        const nomeDoArquivo = src.split("/").pop();
+        const imagem = require(`assets/posts/${post.id}/${nomeDoArquivo}`);
+        return <img src={imagem} alt={alt} />;
+      } catch {
+        return (
+          <span style={{ color: "red" }}>Imagem não encontrada: {src}</span>
+        );
+      }
+    },
+  };
 
   return (
     <PostModel
       fotoCapa={require(`assets/posts/${post.id}/capa.png`)}
       titulo={post.titulo}
     >
-      {post.texto}
+      <div className="post-markdown-container">
+        <ReactMarkdown components={componentesMarkdown}>
+          {post.texto}
+        </ReactMarkdown>
+      </div>
     </PostModel>
   );
 }
